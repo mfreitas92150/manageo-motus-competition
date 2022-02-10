1800afdfbf89e08e3a9a6bbebf0255bfc6b83963
Miguel Freitas <m.freitas@manageo.fr>
2022-02-10 10:26:22 +0100

    ajout auth0 sso

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 
 import React from "react";
+
+import { useAuth0 } from "@auth0/auth0-react";
+
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const [data, setData] = React.useState(null);
+  const { loginWithRedirect, logout, isAuthenticated, isLoading, user } = useAuth0();
+
+
 
   React.useEffect(() => {
     fetch("/api")
@@ -12,12 +18,22 @@ function App() {
       .then((data) => setData(data.message));
   }, []);
 
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
       </header>
+      {!isAuthenticated && <button onClick={() => loginWithRedirect()}>Log In</button>}
+      {isAuthenticated && <button onClick={() => logout({ returnTo: window.location.origin })}>Log Out</button>}
+      {isAuthenticated && (
+        <div>
+          <img src={user.picture} alt={user.name} />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      )}
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { getConfig } from "./config";
+
+const config = getConfig();
+
+const providerConfig = {
+  domain: config.domain,
+  clientId: config.clientId,
+  ...(config.audience ? { audience: config.audience } : null),
+  redirectUri: window.location.origin
+};
+
+console.info(providerConfig)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider {...providerConfig}  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Auth0Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
